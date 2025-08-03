1680c209c20f5290081959cd8b5bdc9e8ebefd2b
mob <mob@Mac.attlocal.net>
2025-08-02 20:52:12 -0700

    api draft: simple example of how to structure the elmlike program
    Run `cabal test` to see the test example.

--- src/ElmLike.hs
+++ src/ElmLike.hs
@@ -1,5 +1,61 @@
-module ElmLike (run) where
+module ElmLike (
+  runProgram,
+  Widget(..),
+) where
 
-run :: IO ()
-run = putStrLn "Run the ElmLike program."
+import Control.Concurrent -- threadDelay
 
+-- Widgets
+data Widget = Text String | Button String String -- TODO button should have some 'onClick' events
+
+data Program model_type command_type = Program {
+  model    :: model_type,
+  updateFn :: model_type -> command_type -> model_type,
+  viewFn   :: model_type -> [Widget]
+}
+
+-- runProgram:
+--   Starts the ElmLike program with the `initialModel` as the starting model.
+--   `upateFn` is triggered when a command is received and the program's model is updated to the
+--   result of the update.
+--   `viewFn` controls the visual representation of the program based on the state.
+runProgram :: model_type -> (model_type -> command_type -> model_type) -> (model_type -> [Widget]) -> command_type -> IO ()
+runProgram initialModel updateFn viewFn cmd = programLifecycleStep (Program {
+  model    = initialModel,
+  updateFn = updateFn,
+  viewFn   = viewFn
+}) cmd
+
+programLifecycleStep :: Program model_type command_type -> command_type -> IO ()
+programLifecycleStep program cmd = do
+  let combined_widgets = map drawWidget ((viewFn program) (model program))
+
+      concatWithSpace :: String -> String -> String
+      concatWithSpace a b = a ++ " " ++ b
+
+      spaceConcat :: [String] -> String
+      spaceConcat lst = (foldl concatWithSpace "") lst
+
+      in putStrLn (spaceConcat combined_widgets)
+
+  -- TODO: Sleeping here for the sake of simulating state change. In this function,
+  -- we are signaling the same command indefinitely to the program's update then sleeping.
+  -- What we want instead is a way for events/actions taken within the program lifecycle
+  -- to emit the corresponding commands that can be used to generate a new state.
+  --
+  -- Need to brainstorm how to go about this implementation.
+  threadDelay 500000
+
+  programLifecycleStep (programRunUpdate program cmd) cmd
+
+programRunUpdate :: Program model_type command_type -> command_type -> Program model_type command_type
+programRunUpdate program cmd = Program {
+  model    = (updateFn program) (model program) cmd,
+  updateFn = (updateFn program),
+  viewFn   = (viewFn program)
+}
+
+drawWidget :: Widget -> String
+drawWidget widget = case widget of
+  (Text body) -> body
+  (Button button_text _) -> button_text
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,27 @@
 module Main (main) where
 
+import ElmLike
+
+data AppState = AppState { counterValue :: Int }
+type Model = AppState
+
+initModel :: Model
+initModel = AppState { counterValue = 0 }
+
+-- The update function needs to be able to take a command,
+-- process the current state and generate a new state based on
+-- that command.
+data Command = Increment | Decrement | None
+updateFn :: Model -> Command -> Model
+updateFn model Increment = AppState { counterValue = (counterValue model) + 1 }
+updateFn model Decrement = AppState { counterValue = (counterValue model) - 1 }
+updateFn model _ = model
+
+viewFn :: Model -> [Widget]
+viewFn model = [Text (show (counterValue model)), Text "more stuff", Text "another element"]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  putStrLn "Running tests..."
+  ElmLike.runProgram initModel updateFn viewFn Increment
+  putStrLn "Tess finished."
