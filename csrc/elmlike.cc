88411d09030ea50e560d22b7f908c03677461f95
mob <mob@Mac.attlocal.net>
2025-08-09 13:43:36 -0700

    vulkan/skia: set the proper vulkan version to initialize the skia renderer.
    ```
    DYLD_PRINT_LIBRARIES=1 \
    DYLD_LIBRARY_PATH="/Users/mob/Repos/MoltenVK/Package/Debug/MoltenVK/dynamic/dylib/macOS:${DYLD_LIBRARY_PATH}" \
            ./build/gui_test
    ```

--- csrc/elmlike.cc
+++ csrc/elmlike.cc
@@ -2,47 +2,52 @@
 
 #include <dlfcn.h>
 
-#include <vector>
+#include <cassert>
 #include <cstdint>
+#include <cstdio>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <queue>
 #include <span>
 #include <string_view>
-#include <utility>
-#include <functional>
-#include <cstdio>
-#include <cassert>
 #include <thread>
 #include <unistd.h>
-#include <queue>
-#include <mutex>
-#include <memory>
+#include <utility>
+#include <vector>
 
 #include "VkBootstrap.h"
 
 #define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
 
-#include <vulkan/vulkan_core.h>
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
+#include "skia/include/core/SkCanvas.h"
+#include "skia/include/core/SkColor.h"
+#include "skia/include/core/SkImage.h"
+#include "skia/include/core/SkImageInfo.h"
+#include "skia/include/core/SkPaint.h"
+#include "skia/include/core/SkRect.h"
+#include "skia/include/core/SkRefCnt.h"
+#include "skia/include/core/SkSurface.h"
+#include "skia/include/gpu/GpuTypes.h"
 #include "skia/include/gpu/ganesh/GrDirectContext.h"
+#include "skia/include/gpu/ganesh/SkSurfaceGanesh.h"
 #include "skia/include/gpu/ganesh/vk/GrVkDirectContext.h"
 #include "skia/include/gpu/vk/VulkanBackendContext.h"
-#include "skia/include/gpu/ganesh/SkSurfaceGanesh.h"
-#include "skia/include/core/SkImage.h"
-#include "skia/include/core/SkRefCnt.h"
-#include "skia/include/core/SkImageInfo.h"
-#include "skia/include/core/SkSurface.h"
 #include "skia/include/gpu/vk/VulkanTypes.h"
-#include "skia/include/gpu/GpuTypes.h"
 
 namespace {
 
 constexpr int kVulkanVersionMajor = 1;
 constexpr int kVulkanVersionMinor = 2;
 constexpr uint32_t kVulkanApiVersionMacroValue = VK_API_VERSION_1_2;
+const char *kVulkanVkMoltenSharedObjectName = "libMoltenVK.dylib";
 const char *kVulkanSharedObjectName = "libvulkan.1.dylib";
 
-enum class EventSignal: int {
+enum class EventSignal : int {
   NONE = 0,
   QUIT,
 };
@@ -75,36 +80,61 @@ void signal_hs_shutdown() {
   _event_queue.push(EventSignal::QUIT);
 }
 
-void run_ui_loop(Renderer& renderer) {
+void run_ui_loop(Renderer &renderer) {
   assert(renderer.window);
   glfwMakeContextCurrent(renderer.window);
   while (!glfwWindowShouldClose(renderer.window)) {
     glfwSwapBuffers(renderer.window);
     glfwPollEvents();
+
+    {
+      // Test draw
+      // TODO: The draw isn't presenting anything onto the window...
+      SkCanvas *canvas = renderer.skia_surface->getCanvas();
+
+      SkPaint paint;
+      paint.setColor(SK_ColorBLUE);
+      paint.setStyle(SkPaint::kFill_Style);
+      paint.setAntiAlias(true);
+
+      SkRect rect = SkRect::MakeXYWH(50, 50, 100, 100);
+
+      canvas->drawRect(rect, paint);
+      renderer.skia_ctx->flushAndSubmit();
+    }
   }
   signal_hs_shutdown();
   printf("Window closed.\n");
 }
 
-PFN_vkVoidFunction VulkanGetProcAddr(const char* function_name,
-    VkInstance instance, VkDevice device) {
+PFN_vkVoidFunction VulkanGetProcAddr(const char *function_name,
+                                     VkInstance instance, VkDevice device) {
   const PFN_vkVoidFunction fn1 = vkGetInstanceProcAddr(instance, function_name);
   const PFN_vkVoidFunction fn2 = vkGetDeviceProcAddr(device, function_name);
-  if (fn1 || fn2) printf("[vkProcAddr] fn: %s fn1: %p fn2: %p\n", function_name, fn1, fn2);
-  if (fn1) return fn1;
-  if (fn2) return fn2;
+  // if (fn1 || fn2) printf("[vkProcAddr] fn: %s fn1: %p fn2: %p\n",
+  // function_name, fn1, fn2);
+  if (fn1)
+    return fn1;
+  if (fn2)
+    return fn2;
 
-  static void* handle = dlopen(kVulkanSharedObjectName, RTLD_LAZY);
-  if (!handle) {
+#if __APPLE__
+  static void *vulkan_lib_handle =
+      dlopen(kVulkanVkMoltenSharedObjectName, RTLD_LAZY);
+#else
+  static void *vulkan_lib_handle = dlopen(kVulkanSharedObjectName, RTLD_LAZY);
+#endif
+  if (!vulkan_lib_handle) {
     fprintf(stderr, "Failed to load libvulkan.\n");
     return nullptr;
   }
-  void *fn3 = dlsym(handle, function_name);
-  printf("[vkProcAddr] fn: %s fn1: %p fn2: %p fn3: %p\n", function_name, fn1, fn2, fn3);
+  void *fn3 = dlsym(vulkan_lib_handle, function_name);
+  // printf("[vkProcAddr] fn: %s fn1: %p fn2: %p fn3: %p\n", function_name, fn1,
+  // fn2, fn3);
   return (PFN_vkVoidFunction)fn3;
 }
 
-int SetupSkia(Renderer& renderer, const int width, const int height) {
+int SetupSkia(Renderer &renderer, const int width, const int height) {
   printf("Setting up skia.\n");
 
   skgpu::VulkanBackendContext bctx;
@@ -123,10 +153,8 @@ int SetupSkia(Renderer& renderer, const int width, const int height) {
   }
 
   SkImageInfo info = SkImageInfo::MakeN32Premul(width, height);
-  sk_sp<SkSurface> surface = SkSurfaces::RenderTarget(
-    skia_ctx.get(),
-    skgpu::Budgeted::kYes,
-    info);
+  sk_sp<SkSurface> surface =
+      SkSurfaces::RenderTarget(skia_ctx.get(), skgpu::Budgeted::kYes, info);
   if (!surface) {
     fprintf(stderr, "Failed to create skia surface.\n");
     return 1;
@@ -139,7 +167,7 @@ int SetupSkia(Renderer& renderer, const int width, const int height) {
   return 0;
 }
 
-int init_window_with_skia(Renderer& renderer) {
+int init_window_with_skia(Renderer &renderer) {
   printf("Starting GUI.\n");
 
   constexpr int kWidth = 800;
@@ -164,8 +192,8 @@ int init_window_with_skia(Renderer& renderer) {
     fprintf(stderr, "Window already initialized.\n");
     return 1;
   }
-  renderer.window = glfwCreateWindow(kWidth, kHeight, "Elmlike",
-    nullptr, nullptr);
+  renderer.window =
+      glfwCreateWindow(kWidth, kHeight, "Elmlike", nullptr, nullptr);
   if (renderer.window == nullptr) {
     fprintf(stderr, "Failed to create window.\n");
     return 1;
@@ -174,16 +202,18 @@ int init_window_with_skia(Renderer& renderer) {
   /// Vulkan Extensions Setup
   vkb::SystemInfo system_info = vkb::SystemInfo::get_system_info().value();
   printf("Available Instance Extensions: \n");
-  for (const auto& extension : system_info.available_extensions) {
+  for (const auto &extension : system_info.available_extensions) {
     printf("\t>%s\n", extension.extensionName);
   }
 
   uint32_t glfw_extension_count = 0;
-  const char** glfw_extensions;
+  const char **glfw_extensions;
   glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
 
-  std::span<const char *> glfw_extensions_span(glfw_extensions, glfw_extension_count);
-  std::vector<const char *> extensions(glfw_extensions_span.begin(), glfw_extensions_span.end());
+  std::span<const char *> glfw_extensions_span(glfw_extensions,
+                                               glfw_extension_count);
+  std::vector<const char *> extensions(glfw_extensions_span.begin(),
+                                       glfw_extensions_span.end());
   printf("Setting up vulkan w/ %ld extensions:\n", extensions.size());
   for (const std::string_view ext_name : extensions) {
     printf("\t> %s\n", std::string(ext_name).c_str());
@@ -192,53 +222,59 @@ int init_window_with_skia(Renderer& renderer) {
   /// Vulkan Setup
   /// 1. Creating the VkInstance.
   vkb::InstanceBuilder builder;
-  auto instance_ret = builder.set_app_name("Elmlike Vk Renderer")
-    .request_validation_layers(true)
-    .use_default_debug_messenger()
-    .enable_extensions(extensions.size(), extensions.data())
-    .build();
+  auto instance_ret =
+      builder.set_app_name("Elmlike Vk Renderer")
+          .request_validation_layers(true)
+          .use_default_debug_messenger()
+          .enable_extensions(extensions.size(), extensions.data())
+          .set_minimum_instance_version(kVulkanVersionMajor,
+                                        kVulkanVersionMinor)
+          .require_api_version(kVulkanVersionMajor, kVulkanVersionMinor)
+          .build();
   if (!instance_ret.has_value()) {
     fprintf(stderr, "vkbootstrap: failed to build instance: %s\n",
-      instance_ret.error().message().c_str());
+            instance_ret.error().message().c_str());
     return 1;
   }
 
   /// 2. Creating the VkSurface
   VkSurfaceKHR surface = VK_NULL_HANDLE;
-  if (glfwCreateWindowSurface(instance_ret.value(), renderer.window, nullptr, &surface)) {
+  if (glfwCreateWindowSurface(instance_ret.value(), renderer.window, nullptr,
+                              &surface)) {
     fprintf(stderr, "Failed to create vulkan surface.\n");
     return 1;
   }
 
   /// 3. Select physical device.
-  const std::vector<const char *> physical_device_extensions {
-    "VK_KHR_get_memory_requirements2",
+  const std::vector<const char *> physical_device_extensions{
+      "VK_KHR_get_memory_requirements2",
   };
 
-  vkb::PhysicalDeviceSelector selector { instance_ret.value() };
-  auto phys_ret = selector.set_surface(surface)
-    .set_minimum_version(kVulkanVersionMajor, kVulkanVersionMinor)
-    .add_required_extensions(physical_device_extensions)
-    .select();
+  vkb::PhysicalDeviceSelector selector{instance_ret.value()};
+  auto phys_ret =
+      selector.set_surface(surface)
+          .set_minimum_version(kVulkanVersionMajor, kVulkanVersionMinor)
+          .add_required_extensions(physical_device_extensions)
+          .select();
   if (!phys_ret.has_value()) {
     fprintf(stderr, "Failed to select vulkan physical device:%s\n",
-      phys_ret.error().message().c_str());
+            phys_ret.error().message().c_str());
     return 1;
   }
 
   /// 4. Select vulkan device
-  vkb::DeviceBuilder device_builder { phys_ret.value() };
+  vkb::DeviceBuilder device_builder{phys_ret.value()};
   auto device_ret = device_builder.build();
   if (!device_ret.has_value()) {
     fprintf(stderr, "Failed to create vulkan device: %s\n",
-      device_ret.error().message().c_str());
+            device_ret.error().message().c_str());
     return 1;
   }
 
   auto graphics_queue_ret = device_ret->get_queue(vkb::QueueType::graphics);
   if (!graphics_queue_ret) {
     fprintf(stderr, "Failed to create vulkan graphics queue: %s\n",
-      graphics_queue_ret.error().message().c_str());
+            graphics_queue_ret.error().message().c_str());
     return 1;
   }
 
@@ -266,9 +302,7 @@ Renderer::~Renderer() {
 
 } // namespace
 
-void start_gui() {
-  _start_gui = true;
-}
+void start_gui() { _start_gui = true; }
 
 EventSignal _poll_event_signal() {
   usleep(100'000);
@@ -281,9 +315,7 @@ EventSignal _poll_event_signal() {
   return sig;
 }
 
-int poll_event_signal() {
-  return static_cast<int>(_poll_event_signal());
-}
+int poll_event_signal() { return static_cast<int>(_poll_event_signal()); }
 
 void UiExec(std::function<void()> hs_entry) {
   printf("[UiExec] starting hs thread.\n");
